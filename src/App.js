c06418495aeae309fbaaa43bf6191bfcd24da764
abbasmojarrad <abbasmojarrad7@gmail.com>
2024-07-10 09:46:14 +0330

    update:Add page routes ,  some components for pages , modal component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { useRoutes } from "react-router-dom";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Header from "./Components/Header/Header";
+import routes from "./routes";
+import "./App.css";
 
-function App() {
-  return <h1>Store CMS</h1>;
+export default function App() {
+  const router = useRoutes(routes);
+  return (
+    <div className="wrapper">
+      <Sidebar />
+      <div className="main">
+        <Header />
+        {router}
+      </div>
+    </div>
+  );
 }
-export default App;
---
+++ src/Components/Comments/Comments.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ErrorBox from '../Errorbox/Errorbox'
+
+export default function Comments() {
+  return (
+    <>
+    <ErrorBox msg="هیچ کامنتی یافت نشد" />
+    </>
+  )
+}
