fa55c832de97e0d825f577103750bf17c5a98e05
abbasmojarrad <abbasmojarrad7@gmail.com>
2024-07-12 09:21:48 +0330

    update:complete Users , Products , Comments , ProductsTable  components and edit modals components

--- src/Components/DetailsModal/DetailsModal.js
+++ src/Components/DetailsModal/DetailsModal.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
 import "./DetailsModal.css";
 
-export default function DetailsModal({ onHide }) {
+export default function DetailsModal({ onHide, children }) {
   useEffect(() => {
     const checkKey = (event) => {
+        console.log(event);
       if (event.keyCode === 27) {
         onHide();
       }
@@ -12,28 +13,12 @@ export default function DetailsModal({ onHide }) {
     window.addEventListener("keydown", checkKey);
 
     return () => window.removeEventListener('keydown', checkKey)
-  },[onHide]);
+  });
 
   return (
     <div className="modal-parent active">
       <div className="details-modal">
-        <table className="cms-table">
-          <thead>
-            <tr>
-              <th>اسم</th>
-              <th>قیمت</th>
-              <th>محبوبیت</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr>
-              <td>لپتاپ</td>
-              <td>۱۲،۰۰۰،۰۰۰</td>
-              <td>91</td>
-            </tr>
-          </tbody>
-        </table>
+        {children}
       </div>
     </div>
   );
--- src/Components/ProductsTable/ProductsTable.js
+++ src/Components/ProductsTable/ProductsTable.js
@@ -4,55 +4,95 @@ import DeleteModal from "./../DeleteModal/DeleteModal";
 import DetailsModal from "./../DetailsModal/DetailsModal";
 import EditModal from "./../EditModal/EditModal";
 import { AiOutlineDollarCircle } from "react-icons/ai";
+import ErrorBox from "../Errorbox/Errorbox";
 
-export default function ProductsTable() {
+export default function ProductsTable({ allProducts, getAllProducts }) {
   const [isShowDeleteModal, setIsShowDeleteModal] = useState(false);
   const [isShowDetailsModal, setIsShowDetailsModal] = useState(false);
   const [isShowEditModal, setIsShowEditModal] = useState(false);
-  const [allProducts, setAllProducts] = useState([]);
+  const [productID, setProductID] = useState(null);
+  const [mainProductInfos, setMainProductInfos] = useState({});
 
-  useEffect(() => {
-    fetch("http://localhost:8000/api/products/")
-      .then((res) => res.json())
-      .then((products) => setAllProducts(products));
-  }, []);
+  const [productNewTitle, setProductNewTitle] = useState("");
+  const [productNewPrice, setProductNewPrice] = useState("");
+  const [productNewCount, setProductNewCount] = useState("");
+  const [productNewImg, setProductNewImg] = useState("");
+  const [productNewPopularity, setProductNewPopularity] = useState("");
+  const [productNewSale, setProductNewSale] = useState("");
+  const [productNewColors, setProductNewColors] = useState("");
 
   const deleteModalCancelAction = () => {
+    console.log("مدال کنسل شد");
     setIsShowDeleteModal(false);
   };
 
   const deleteModalSubmitAction = () => {
-    setIsShowDeleteModal(false);
+    console.log("مدال تایید شد");
+    fetch(`http://localhost:8000/api/products/${productID}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setIsShowDeleteModal(false);
+        getAllProducts();
+      });
   };
 
   const closeDetailsmodal = () => {
     setIsShowDetailsModal(false);
+    console.log("مدال جزییات بسته شد");
   };
 
   const updateProductInfos = (event) => {
     event.preventDefault();
+
+    const productsNewInfos = {
+      title: productNewTitle,
+      price: productNewPrice,
+      count: productNewCount,
+      img: productNewImg,
+      popularity: productNewPopularity,
+      sale: productNewSale,
+      colors: productNewColors,
+    };
+
+    fetch(`http://localhost:8000/api/products/${productID}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productsNewInfos),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        getAllProducts();
+        setIsShowEditModal(false);
+      });
+
+    console.log("محصول ویرایش شد");
   };
 
   return (
     <>
-      <table className="products-table">
-        <thead>
-          <tr className="products-table-heading-tr">
-            <th>عکس</th>
-            <th>اسم</th>
-            <th>قیمت</th>
-            <th>موجودی</th>
-          </tr>
-        </thead>
+      {allProducts.length ? (
+        <table className="products-table">
+          <thead>
+            <tr className="products-table-heading-tr">
+              <th>عکس</th>
+              <th>اسم</th>
+              <th>قیمت</th>
+              <th>موجودی</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          {allProducts.map((product) => {
-            return (
+          <tbody>
+            {allProducts.map((product) => (
               <tr key={product.id} className="products-table-tr">
                 <td>
                   <img
                     src={product.img}
-                    alt={product.img}
+                    alt="product img"
                     className="products-table-img"
                   />
                 </td>
@@ -62,35 +102,75 @@ export default function ProductsTable() {
                 <td>
                   <button
                     className="products-table-btn"
-                    onClick={() => setIsShowDetailsModal(true)}
+                    onClick={() => {
+                      setIsShowDetailsModal(true);
+                      setMainProductInfos(product);
+                    }}
                   >
                     جزییات
                   </button>
                   <button
                     className="products-table-btn"
-                    onClick={() => setIsShowDeleteModal(true)}
+                    onClick={() => {
+                      setIsShowDeleteModal(true);
+                      setProductID(product.id);
+                    }}
                   >
                     حذف
                   </button>
                   <button
                     className="products-table-btn"
-                    onClick={() => setIsShowEditModal(true)}
+                    onClick={() => {
+                      setIsShowEditModal(true);
+                      setProductID(product.id);
+                      setProductNewTitle(product.title);
+                      setProductNewPrice(product.price);
+                      setProductNewCount(product.count);
+                      setProductNewImg(product.img);
+                      setProductNewPopularity(product.popularity);
+                      setProductNewSale(product.sale);
+                      setProductNewColors(product.colors);
+                    }}
                   >
                     ویرایش
                   </button>
                 </td>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <ErrorBox msg="هیچ محصولی یافت نشد" />
+      )}
+
       {isShowDeleteModal && (
         <DeleteModal
+          title="آیا از حذف اطمینان دارید؟"
           submitAction={deleteModalSubmitAction}
           cancelAction={deleteModalCancelAction}
         />
       )}
-      {isShowDetailsModal && <DetailsModal onHide={closeDetailsmodal} />}
+      {isShowDetailsModal && (
+        <DetailsModal onHide={closeDetailsmodal}>
+          <table className="cms-table">
+            <thead>
+              <tr>
+                <th>محبوبیت</th>
+                <th>فروش</th>
+                <th>رنگ بندی</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>{mainProductInfos.popularity}%</td>
+                <td>{mainProductInfos.sale}</td>
+                <td>{mainProductInfos.colors}</td>
+              </tr>
+            </tbody>
+          </table>
+        </DetailsModal>
+      )}
       {isShowEditModal && (
         <EditModal
           onClose={() => setIsShowEditModal(false)}
@@ -104,6 +184,21 @@ export default function ProductsTable() {
               type="text"
               placeholder="عنوان جدید را وارد کنید"
               className="edit-product-input"
+              value={productNewTitle}
+              onChange={(event) => setProductNewTitle(event.target.value)}
+            />
+          </div>
+
+          <div className="edit-proructs-form-group">
+            <span>
+              <AiOutlineDollarCircle />
+            </span>
+            <input
+              type="text"
+              placeholder="قیمت جدید را وارد کنید"
+              className="edit-product-input"
+              value={productNewPrice}
+              onChange={(event) => setProductNewPrice(event.target.value)}
             />
           </div>
           <div className="edit-proructs-form-group">
@@ -112,8 +207,10 @@ export default function ProductsTable() {
             </span>
             <input
               type="text"
-              placeholder="عنوان جدید را وارد کنید"
+              placeholder="موجودی جدید را وارد کنید"
               className="edit-product-input"
+              value={productNewCount}
+              onChange={(event) => setProductNewCount(event.target.value)}
             />
           </div>
           <div className="edit-proructs-form-group">
@@ -122,8 +219,10 @@ export default function ProductsTable() {
             </span>
             <input
               type="text"
-              placeholder="عنوان جدید را وارد کنید"
+              placeholder="آدرس کاور جدید را وارد کنید"
               className="edit-product-input"
+              value={productNewImg}
+              onChange={(event) => setProductNewImg(event.target.value)}
             />
           </div>
           <div className="edit-proructs-form-group">
@@ -132,8 +231,34 @@ export default function ProductsTable() {
             </span>
             <input
               type="text"
-              placeholder="عنوان جدید را وارد کنید"
+              placeholder="محبوبیت جدید را وارد کنید"
               className="edit-product-input"
+              value={productNewPopularity}
+              onChange={(event) => setProductNewPopularity(event.target.value)}
+            />
+          </div>
+          <div className="edit-proructs-form-group">
+            <span>
+              <AiOutlineDollarCircle />
+            </span>
+            <input
+              type="text"
+              placeholder="میزان فروش جدید را وارد کنید"
+              className="edit-product-input"
+              value={productNewSale}
+              onChange={(event) => setProductNewSale(event.target.value)}
+            />
+          </div>
+          <div className="edit-proructs-form-group">
+            <span>
+              <AiOutlineDollarCircle />
+            </span>
+            <input
+              type="text"
+              placeholder="تعداد رنگ بندی جدید را وارد کنید"
+              className="edit-product-input"
+              value={productNewColors}
+              onChange={(event) => setProductNewColors(event.target.value)}
             />
           </div>
         </EditModal>
